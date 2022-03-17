41522babf17a3776893eee44d47726765fcd27a1
Victor Senna <vhsenna@gmail.com>
2022-03-16 22:55:58 -0300

    Insert fake data

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.faker import make_recipe
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'recipes': [make_recipe() for _ in range(13)],
+    })
 
 def recipe(request, id):
-    return render(request, 'recipes/recipe.html')
+    return render(request, 'recipes/recipe.html', context={
+        'recipe': make_recipe(),
+    })
