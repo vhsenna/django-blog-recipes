4270f12f6b937edbf0dc08bf3c6234450bbb4257
Victor Senna <vhsenna@gmail.com>
2022-03-18 07:58:45 -0300

    Update models

--- recipes/models.py
+++ recipes/models.py
@@ -1,11 +1,14 @@
-from random import choices
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
-    slug = models.SlugField())
+    slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=50)
     servings = models.IntegerField()
@@ -16,3 +19,5 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/images/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
