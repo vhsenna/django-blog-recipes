2f78a7bb18ba1d7c2b34638627d084c63f73ba51
Victor Senna <vhsenna@gmail.com>
2022-03-18 00:44:46 -0300

    Create model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,18 @@
+from random import choices
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    slug = models.SlugField())
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=50)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=50)
+    preparation_step = models.TextField()
+    preparation_step_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/images/%Y/%m/%d/')
