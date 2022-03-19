419f017a26725302b9ce2efdbb5dae9b2198ed06
Victor Senna <vhsenna@gmail.com>
2022-03-19 02:55:57 -0300

    Add models in admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
@@ -21,3 +24,6 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/images/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
