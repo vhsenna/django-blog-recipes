5ffe57fdc7ef00684e6a87dfbd80a5959c3aaffb
Victor Senna <vhsenna@gmail.com>
2022-03-19 22:31:50 -0300

    Fix models in admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Category, Recipe
 
 # Register your models here.
-admin.register(Category)
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
