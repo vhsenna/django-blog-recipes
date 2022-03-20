72a5e8bf31e4edc237224463446430fa653247d1
Victor Senna <vhsenna@gmail.com>
2022-03-19 22:56:15 -0300

    Change media repository

--- project/settings.py
+++ project/settings.py
@@ -121,6 +121,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
