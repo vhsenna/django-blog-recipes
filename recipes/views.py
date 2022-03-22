c213f2eeadc05443480fc298ee5c6f1927a11241
Victor Senna <vhsenna@gmail.com>
2022-03-21 22:02:36 -0300

    Use shortcut get_object_or_404()

--- recipes/views.py
+++ recipes/views.py
@@ -1,35 +1,23 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.faker import make_recipe
 from recipes.models import Recipe
 
 # Create your views here.
 def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
-
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/home.html', context={
         'recipes': recipes,
     })
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id,
-        is_published=True,
-    ).order_by('-id').first()
-
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/recipe.html', context={
         'recipe': recipe,
         'is_detail_page': True,
     })
 
 def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-        ).order_by('-id')
-    )
-
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
     return render(request, 'recipes/home.html', context={
         'recipes': recipes,
     })
