e73122643fcce51c6141f4638800391859885838
Victor Senna <vhsenna@gmail.com>
2022-03-26 19:18:45 -0300

    Test Django Reverse URL with args and kwargs

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
 class RecipeURLsTest(TestCase):
 
-    def test_pytest_is_work(self):
-        ...
+    def test_recipe_home_url_is_working(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_id_url_is_working(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipe/1/')
+
+    def test_recipe_category_url_is_working(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipe/category/1/')
