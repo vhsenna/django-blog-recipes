a89c2d8a82cbbb1662c804c4e752a649c55a2dd6
Victor Senna <vhsenna@gmail.com>
2022-03-28 22:01:00 -0300

    Create a test package

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -2,20 +2,6 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
 
-class RecipeURLsTest(TestCase):
-
-    def test_recipe_home_url_is_working(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_id_url_is_working(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipe/1/')
-
-    def test_recipe_category_url_is_working(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipe/category/1/')
-
 class RecipeViewsTest(TestCase):
 
     def test_recipe_home_view_function_is_working(self):
